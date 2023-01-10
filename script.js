33f4e84877bc7d7d5c132f17ee7c3765d897eda8
Andrea <andrearivas4cinfor@gmail.com>
2023-01-10 09:39:28 +0100

    fix local storage function

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ const addBtn = document.querySelector('.add-book-btn');
 const newTitleInput = document.querySelector('.add-title-input');
 const newAuthorInput = document.querySelector('.add-author-input');
 const libraryContainer = document.querySelector('.library-wrapper');
+
 let library = [];
+
 const Book = function (id, title, author) {
   this.id = id;
   this.title = title;
   this.author = author;
 };
+
+// Local Storage
+
+const getBook = JSON.parse(localStorage.getItem('library'));
+if (getBook === 0 || getBook === null) {
+  localStorage.setItem('book', JSON.stringify(library));
+} else {
+  library = getBook;
+}
+
 // Function to add and display new books.
 const addBook = function (e) {
   e.preventDefault();
@@ -21,7 +33,7 @@ const addBook = function (e) {
     newId = library[len - 1].id + 1;
   }
   if (newTitle && newAuthor) {
-    const newBook = new Book(newId,newTitle, newAuthor);
+    const newBook = new Book(newId, newTitle, newAuthor);
     library.push(newBook);
     const html = ` <div class="book-wrapper">
   <p class="title-display">${newTitle}</p>
@@ -30,17 +42,19 @@ const addBook = function (e) {
   <div class="grey-line"></div>
 </div>`;
     libraryContainer.innerHTML += html;
+    localStorage.setItem('book', JSON.stringify(library)); // LOCAL STORAGE
   }
 };
-//Function to remove new Book
-const removeBook = function(e) {
-  if(e.target.classList.contains('remove-btn')){
+
+// Function to remove new Book
+const removeBook = function (e) {
+  if (e.target.classList.contains('remove-btn')) {
     const { id } = e.target;
     library = library.filter((bk) => JSON.stringify(bk.id) !== id);
-    localStorage.setItem('local', JSON.stringify(library)); // LOCAL STORAGE
+    localStorage.setItem('book', JSON.stringify(library)); // LOCAL STORAGE
     e.target.parentElement.remove();
   }
-}
+};
 // Event Listeners
 addBtn.addEventListener('click', addBook);
-libraryContainer.addEventListener('click',removeBook);
+libraryContainer.addEventListener('click', removeBook);
