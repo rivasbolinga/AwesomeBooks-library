037c973ad1bf34ff85bc231b65fa4ab724914535
Andrea <andrearivas4cinfor@gmail.com>
2023-01-10 17:42:51 +0100

    Add local Storage and UI classes

--- script.js
+++ script.js
@@ -2,24 +2,61 @@ const addBtn = document.querySelector('.add-book-btn');
 const newTitleInput = document.querySelector('.add-title-input');
 const newAuthorInput = document.querySelector('.add-author-input');
 const libraryContainer = document.querySelector('.library-wrapper');
+let newId = 0;
 
-const store = JSON.parse(localStorage.getItem('book'));
+//class constructor of the book object
+class Book {
+  constructor(title,author,id) {
+    this.author = author;
+    this.title = title;
+    this.id = id;
+  }
+}
 
-let library = store || [];
+//Local Storage
 
-// Display book from local storage
+class Storage {
+  static getbooks() {
+    let books;
+    if(localStorage.getItem('book') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(getItem('books'));
+    }
+    return books
+  }
+  static addBook(book) {
+    const books = Storage.getbooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+  
+}
 
-for (let i = 0; i < library.length; i += 1) {
-  const html = `
+class UI {
+  static displayBook() {
+    const books = Storage.getbooks();
+    books.forEach((newBook) => {
+      UI.addBooktoLibrary(newBook);
+    });
+  }
+
+  static addBooktoLibrary(newBook){
+    newBook.id = newId;
+    const html = `
     <div>
-      <p class="book-position">${library[i].title}</p>
-      <p class="book-title">${library[i].author}</p>
-      <button id="${library[i].id}" class="remove-btn">Remove</button>
+      <p class="book-position">${newBook.title}</p>
+      <p class="book-title">${newBook.author}</p>
+      <button id="${newBook.id}" class="remove-btn">Remove</button>
       <div class="line-bottom"></div>
     </div>
   `;
   libraryContainer.innerHTML += html;
+  newId += 1;
+  }
 }
+
+  
 // display new book
 
 const displayBook = function (newBook) {
@@ -32,45 +69,3 @@ const displayBook = function (newBook) {
   libraryContainer.innerHTML += html;
   localStorage.setItem('book', JSON.stringify(library)); // LOCAL STORAGE
 };
-
-const Book = function (id, title, author) {
-  this.id = id;
-  this.title = title;
-  this.author = author;
-};
-
-// Function to add and display new books.
-
-const addBook = function (e) {
-  e.preventDefault();
-  const newTitle = newTitleInput.value;
-  const newAuthor = newAuthorInput.value;
-  let newId;
-  const len = library.length;
-  if (len === 0 || len === null) {
-    newId = 0;
-  } else {
-    newId = library[len - 1].id + 1;
-  }
-  if (newTitle && newAuthor) {
-    const newBook = new Book(newId, newTitle, newAuthor);
-    library.push(newBook);
-    displayBook(newBook);
-  }
-};
-
-// Function to remove new Book
-
-const removeBook = function (e) {
-  if (e.target.classList.contains('remove-btn')) {
-    const { id } = e.target;
-    library = library.filter((bk) => JSON.stringify(bk.id) !== id);
-    localStorage.setItem('book', JSON.stringify(library)); // LOCAL STORAGE
-    e.target.parentElement.remove();
-  }
-};
-
-// Event Listeners
-
-addBtn.addEventListener('click', addBook);
-libraryContainer.addEventListener('click', removeBook);
